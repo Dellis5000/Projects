a06c49c51a9996279acd110c84ddaa4fa1060a2d
Dellis5000 <56387527+Dellis5000@users.noreply.github.com>
2019-11-26 17:43:35 +0000

    New

---
+++ softwareengdatabase-master/Software Engin Project/softwareengdatabase-master/Software Engin Project/Software Engin Project/BedOverview.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_Engin_Project
+{
+    public partial class BedOverview : Form
+    {
+        public BedOverview()
+        {
+            InitializeComponent();
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            string time = Constants.TimeStamp(DateTime.Now);
+
+            int employeeID = Convert.ToInt32(Constants.currentEmployee);
+
+            DatabaseConnection.Sample.loginRecord(employeeID, time);
+
+            Login log = new Login();
+
+            log.Show();
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Management man = new Management();
+
+            man.Show();
+        }
+
+        private void PictureBox3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Constants.currentBed = 0;
+            BedsideMonitoring bed = new BedsideMonitoring();
+            
+            bed.Show();
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Constants.currentBed = 1;
+            BedsideMonitoring bed = new BedsideMonitoring();
+            
+            bed.Show();
+        }
+
+        private void BedOverview_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox11_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Constants.currentBed = 2;
+            BedsideMonitoring bed = new BedsideMonitoring();
+            
+            bed.Show();
+        }
+
+        private void PictureBox10_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Constants.currentBed = 3;
+            BedsideMonitoring bed = new BedsideMonitoring();
+            
+            bed.Show();
+        }
+
+        private void PictureBox9_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Constants.currentBed = 4;
+            BedsideMonitoring bed = new BedsideMonitoring();
+           
+            bed.Show();
+        }
+
+        private void PictureBox8_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Constants.currentBed = 5;
+            BedsideMonitoring bed = new BedsideMonitoring();
+            
+            bed.Show();
+        }
+
+        private void PictureBox6_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Constants.currentBed = 6;
+            BedsideMonitoring bed = new BedsideMonitoring();
+            
+            bed.Show();
+        }
+
+        private void PictureBox7_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Constants.currentBed = 7;
+            BedsideMonitoring bed = new BedsideMonitoring();
+            
+            bed.Show();
+        }
+    }
+}
---
+++ softwareengdatabase-master/Software Engin Project/softwareengdatabase-master/Software Engin Project/Software Engin Project/BedsideMonitoring.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Software_Engin_Project
+{
+    public partial class BedsideMonitoring : Form
+    {
+        public static void ThreadProc()
+        {
+            for (int i = 0; i < 999999; i++)
+            {
+                
+                // Yield the rest of the time slice.
+                Thread.Sleep(0);
+            }
+        }
+        public BedsideMonitoring()
+        {
+            Thread t = new Thread(new ThreadStart(ThreadProc));
+            t.Start();
+            InitializeComponent();
+            if (RunningData.beds[Constants.currentBed].currentPatient == null)
+            {
+                Namelabel.Text = "No Patient";
+                return;
+            }
+            else
+            {
+                string FirstName = RunningData.beds[Constants.currentBed].currentPatient.Firstname;
+                string LastName = RunningData.beds[Constants.currentBed].currentPatient.LastName;
+                Namelabel.Text =   FirstName + LastName;
+
+                Thread thr = new Thread(new ThreadStart(AssignVitals));
+                thr.Start();
+                
+            }            
+
+        }
+        public void AssignVitals()
+        {
+            for (int i = 0; i < 99999999; i++)
+            {
+                if (RunningData.beds[Constants.currentBed].currentPatient == null)
+                {
+                    break;
+                }
+                else
+                {
+
+                    string pulse = RunningData.beds[Constants.currentBed].currentPatient.Pulse.ToString("00.00");
+                    if (PulseText.InvokeRequired)
+                        this.Invoke(new MethodInvoker(() => PulseText.Text = pulse));
+                    else
+                        PulseText.Text = pulse;
+
+                    string breath = RunningData.beds[Constants.currentBed].currentPatient.Breathing.ToString("00.00");
+                    if (PulseText.InvokeRequired)
+                        this.Invoke(new MethodInvoker(() => BreathingText.Text = breath));
+                    else
+                        BreathingText.Text = breath;
+
+                    string temp = RunningData.beds[Constants.currentBed].currentPatient.Temp.ToString("00.00");
+                    if (TempText.InvokeRequired)
+                        this.Invoke(new MethodInvoker(() => TempText.Text = temp));
+                    else
+                        TempText.Text = pulse;
+
+                    string blood = RunningData.beds[Constants.currentBed].currentPatient.Blood.ToString("00.00");
+                    if (Bloodtext.InvokeRequired)
+                        this.Invoke(new MethodInvoker(() => Bloodtext.Text = blood));
+                    else
+                        Bloodtext.Text = pulse;
+
+                    Thread.Sleep(0);
+                }
+            }
+        }
+
+        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            BedOverview bed = new BedOverview();
+
+            bed.Show();
+        }
+
+        private void BedsideMonitoring_Load(object sender, EventArgs e)
+        {
+            
+        }
+           
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BreathingText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label10_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
